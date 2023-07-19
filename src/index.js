47df5af318bfc31a215245e8a83f2ef818b21c41
Tunahan Bayır <tunahanbayir@192.168.1.6>
2023-07-19 23:18:01 +0300

    Tamamlandı

--- src/index.js
+++ src/index.js
@@ -7,3 +7,60 @@ import './less/index.less'
 
 
 // Kodlar buraya gelecek!
+
+
+document.querySelector(".logo-heading").addEventListener("mouseover", e => {e.target.style.color = "blue";});
+
+
+document.querySelector(".logo-heading").addEventListener("mouseout", e => {e.target.style.color = "black";});
+
+
+const rota = document.querySelectorAll("h4");
+rota.forEach((h4) => {
+    h4.addEventListener("mousemove", () => {
+        h4.style.color = "red";
+    })
+});
+
+
+const headerDOM = document.querySelector(".main-navigation");
+const bodyStyle = () => (headerDOM.style.backgroundColor = "pink");
+document.body.addEventListener("wheel", bodyStyle);
+
+
+document.addEventListener("keydown", (e) => {
+    if (e.shiftKey) {
+        const başlık = document.querySelector("h1");
+        başlık.style.color = "purple";
+    }
+});
+
+document.querySelector(".content-destination img").addEventListener("dblclick", (e) => {
+    if (e.target.style.borderRadius === "100%") {
+        e.target.style.borderRadius === "";  
+    } else {
+        e.target.style.borderRadius === "100%";
+    }
+});
+
+
+const form = document.querySelectorAll(".nav-link");
+const formArr = Array.from(form);
+formArr.forEach((element) => {
+    element.addEventListener("focus", (e) => {
+        e.target.style.background = "navy";
+    });
+    element.addEventListener("blur", (e) => {
+        e.target.style.background = "gray";
+    });
+});
+
+const loading = document.querySelectorAll("img");
+loading[1].addEventListener("load", (e) => {
+    alert("Yüklendi");
+});
+
+window.addEventListener("scroll", () => {
+    console.log("Sayfa kaydırıldı");
+});
+
